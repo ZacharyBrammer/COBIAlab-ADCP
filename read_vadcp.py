3dcadc293a25086f53078c3d11611633ba85d186
ZacharyBrammer <zacharyj.brammer@gmail.com>
2025-06-30 12:45:05 -0400

    Organize Code, Add Exception Handling

--- read_vadcp.py
+++ read_vadcp.py
@@ -1,29 +1,29 @@
-#!/usr/pbin/python
-#Read VADCP data from serial port and save to file
+# Read VADCP data from serial port and save to file
 
 import datetime
+import time
 
 import serial
 
-#print "Data written at :", str(datetime.datetime.now())
+while True:
+    try:
+        # open data incoming serial port
+        ser1in = serial.Serial('/dev/ttyUSB0', 115200)
+        print("Opening vadcp serial port: ", ser1in.name)
 
-ser1in = serial.Serial('/dev/ttyUSB0',115200) #open data incoming serial port
-print("Opening vadcp serial port: ", ser1in.name) #check correct serial port was opened
+        datafile = '/home/cobialab/measurements/measurements.000'
+        vadcp1 = open(datafile, 'ab')  # open file for writing
 
-#ser1out = serial.Serial('/dev/ttyUSB1',4800) #open send data port
-#print "Opening send data serial port: ", ser1out.name
+        read_byte1 = ser1in.read()
+        count = 0
+        while read_byte1 is not None:  # loop over serial port
+            read_byte1 = ser1in.read()  # read data from serial port
+            vadcp1.write(read_byte1)  # write data to file
+            vadcp1.close()  # close file
+            vadcp1 = open(datafile, 'ab')  # reopen file
+            print("Data written at :", str(
+                datetime.datetime.now()), "to ", str(vadcp1.name))
 
-datafile='/home/cobialab/measurements/measurements.000'
-
-vadcp1 = open(datafile,'ab') #open file for writing
-
-read_byte1 = ser1in.read() 
-count = 0
-while read_byte1 is not None:  #loop over serial port
-	read_byte1 = ser1in.read() #read data from serial port
-	vadcp1.write(read_byte1) #write data to file
-	vadcp1.close() #close file
-	vadcp1 = open(datafile,'ab') #reopen file
-	print("Data written at :", str(datetime.datetime.now()),"to ",str(vadcp1.name))
-	#ser1out.write(read_byte1)
-
+    except serial.SerialException as e:
+        print(e)
+        time.sleep(5)
